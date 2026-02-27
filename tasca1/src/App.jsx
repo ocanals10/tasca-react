91f19776c8cd1b21f3cdbc859c7610aab61d4198
Oriol Canals <ocanals10@xarxa.fedac.cat>
2026-02-27 19:19:49 +0100

    tasca_react

---
+++ tasca1/src/App.jsx
@@ -0,0 +1,13 @@
+import Exercici2 from './components/tasca1/exercici2'
+
+function App() {
+
+
+  return (
+
+      <Exercici2 />
+    
+  )
+}
+
+export default App
---
+++ tasca1/src/components/tasca1/exercici2.jsx
@@ -0,0 +1,56 @@
+import {useState} from "react";
+import './exercici2.css'
+
+function Exercici2() {
+    const [cercle, background, color] =useState(0)
+
+    function incrementar() {
+        if (titol.style.color == "red"){
+            titol.style.color = "black"
+        }
+  }
+
+    function decrementar() {
+        if (cercle != "yellow") background.color = "yellow";
+  }
+
+    function reset() {
+        if (comptador > 0) background.color = "green";
+        else if (comptador < 0) color = "red";
+        else color = "black";
+  }
+    
+  return (
+
+    <body>
+
+    <div className="block">
+      <div className="block1">
+        
+        <div className="cercle">
+
+        </div>
+
+        <div className="select">
+          <div className="descrementar">
+            <button onClick={decrementar}>Decrementar</button>
+          </div>
+
+          <div className="reiniciar">
+            <button onClick={reset}>Reset</button>
+          </div>
+
+          <div className="incrementar">
+            <button onClick={incrementar}>Incrementar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+    <script src="script.js"></script>
+</body>
+
+  )
+}
+
+export default Exercici2
