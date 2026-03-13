d48e769959d8afffc485174c3470d90fa41b9b29
Oriol Canals <ocanals10@xarxa.fedac.cat>
2026-03-13 19:35:46 +0100

    aaa

--- src/components/tasca1/exercici3.jsx
+++ src/components/tasca1/exercici3.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 
 function Exercici3() {
-  // 1. Definim els estats per a cada estil
   const [isBold, setIsBold] = useState(false);
   const [isItalic, setIsItalic] = useState(false);
   const [isUnderlined, setIsUnderlined] = useState(false);
 
-  // 2. Creem un objecte d'estils que depèn de l'estat
   const textStyle = {
     fontWeight: isBold ? "bold" : "normal",
     fontStyle: isItalic ? "italic" : "normal",
@@ -19,11 +17,9 @@ function Exercici3() {
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       <h2>Formatador de Text</h2>
 
-      {/* El text que canvia segons els estats */}
       <p style={textStyle}>Lorem Ipsum</p>
 
       <div className="buttons">
-        {/* En fer clic, invertim l'estat actual amb l'operador NOT (!) */}
         <button onClick={() => setIsBold(!isBold)}>
           {isBold ? "Treure Negreta" : "Negreta"}
         </button>
